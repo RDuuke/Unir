2c01ff20e10437edc69ec72c9e2f6908fd4934c8
RDuuke <juuanduuke@gmail.com>
2019-08-05 21:47:33 -0500

    first commit

---
+++ bootstart/define.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RDuuke
+ * Date: 5/08/2019
+ * Time: 9:39 PM
+ */
---
+++ src/router.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RDuuke
+ * Date: 5/08/2019
+ * Time: 9:41 PM
+ */
